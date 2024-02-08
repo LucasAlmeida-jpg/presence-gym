e0af9e67a3ae30c867319fa9550cccfe1968a108
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-02-08 20:06:18 -0300

    inclusion user dashboard

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,6 +1,42 @@
-<script >
-</script>
-
 <template>
-    area do usuario
+    <section class="container-0">
+        <nav class="row px-5 py-3 d-flex align-items-center justify-content-between">
+            <div class="col-md-6">
+                <h5>
+                    Nome: Lucas
+                </h5>
+                <h5>
+                    Faixa: Preta
+                </h5>
+            </div>
+            <div class="col-md-6"></div>
+        </nav>
+        <article class="row article px-5 py-3 text-center mt-3 justify-content-center d-flex">
+            <h3 class="text-center">Área de Presença do Aluno</h3>
+        </article>
+
+        <article class="row article px-5 py-3 text-center mt-3">
+            <div class="col-md-6 mb-3">
+                <div class="card">
+                    <h3>Presença</h3>
+                    <h4>5</h4>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <h3>Faltas</h3>
+                    <h4>2</h4>
+                </div>
+            </div>
+        </article>
+
+        <footer class="row px-5 py-3 d-flex align-items-center justify-content-between">
+            <div class="col-md-6">
+                <div>
+                   <button class="btn">Histórico</button class="btn">  
+                </div>
+            </div>
+            <div class="col-md-6"></div>
+        </footer>
+    </section>
 </template>
